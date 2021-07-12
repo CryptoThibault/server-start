92be96593b7e1d65ff3c5425d0e4f466db06382d
CryptoThibault <thibault.chalaoux@gmail.com>
2021-07-12 16:53:23 +0200

    register

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 const express = require('express')
-
+const fsPromises = require('fs/promises')
 const app = express()
-const port = 3333
 
+const PORT = 3333
+const IP_LOOPBACK = 'localhost'
+const LOG_FILE = 'login.json'
 
 app.get('/', async (req, res ) => {
-  res.send(`Welcome to your local server at port ${port}`)
+  res.send(`Welcome to your local server at PORT ${PORT}`)
+})
+
+app.get('/hello/:name', async (req, res) => {
+  res.send(`Hello ${req.params.name}!`)
+})
+
+app.get('/register/:name/:password', async (req, res) => {
+  try {
+    const log = { name: req.params.name, password: req.params.password }
+    await fsPromises.appendFile(LOG_FILE, JSON.stringify(log), 'utf-8')
+    res.send(`You register as ${req.params.name}`)
+  } catch (e) {
+    console.error(`Error: can't write in ${LOG_FILE}`)
+  }
+})
+app.get('/login/:name/:password', async (req, res) => {
+  try {
+    const log = await fsPromises.readFile(LOG_FILE, 'utf-8')
+    log.forEach(el => {
+      if (el.name === req.params.name && el.password === req.params.password) {
+        res.send(`You login as ${req.params.name}`)
+      }
+    });
+  } catch (e) {
+    console.log(`Error: can't read in ${LOG_FILE}`)
+  }
+})
+
+app.listen(PORT, async () => {
+  console.log(`App listening at http://${IP_LOOPBACK}:${PORT}`)
 })
